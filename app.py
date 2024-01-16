bd5346075882d4b70152cd7a6530e08585f60ff5
David <davidhy8@gmail.com>
2024-01-15 21:57:35 -0700

    commit and push

--- app.py
+++ app.py
@@ -18,3 +18,4 @@ print("Hi")
 # General outline of project:
 # i) Front-end: Website to display data (NBA team win & loss predictions, top fantasy scorers of the night)
 # ii) Back-end: Machine learning to make predictions, Algorithms to fetch data, NLP to check headlines for injury
+# Git commit and push
