8bc087c8a2c6a10a24aadf9db557f9a2e545e53f
davidhy8 <david.yang1@ucalgary.ca>
2024-01-20 21:36:36 -0700

    added webscraping and data cleaning scripts

--- app.py
+++ app.py
@@ -9,13 +9,3 @@ if __name__ == '__main__':
     app.run()
 
 print("Hi")
-
-# TO DO LIST:
-# 1. Familiarize with the use of Github
-# 2. Learn how to create a Flask project. You will also have to learn basic HTML, CSS, and Javascript
-# 3. Create a framework for the project, and then we will fill in each file with code
-
-# General outline of project:
-# i) Front-end: Website to display data (NBA team win & loss predictions, top fantasy scorers of the night)
-# ii) Back-end: Machine learning to make predictions, Algorithms to fetch data, NLP to check headlines for injury
-# Testing
---
+++ web_scraper.py
@@ -0,0 +1,4 @@
+import os
+from bs4 import BeautifulSoup
+from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeout
+
